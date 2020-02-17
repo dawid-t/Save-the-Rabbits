f375425aa03bf88ed4cf31239de6f1668984f078
dawid-t <dawiddt@wp.pl>
2020-02-17 22:10:29 +0100

    Create a test scene with a 2.5D terrain
    The terrain is created from some meshes (used ProBuilder).
    Added a MeshCutter too. It is a tool that can cut/split meshes.

---
+++ Assets/Scripts/MeshCutter/TerrainCutter.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+
+public class TerrainCutter : MonoBehaviour
+{
+	public GameObject gameObjectToCut;
+
+
+	private void Start()
+	{
+		CutMesh();
+	}
+
+	private void Update()
+	{
+
+	}
+
+	public void CutMesh()
+	{
+		/*
+		 * 1. Pobierz dotknietego mesha
+		 * 2. Przetnij go w miejscu gdzie byl dotkniety
+		 *
+		 * ?. Przetnij mesha w pół
+		 */
+		MeshFilter meshFilter = gameObjectToCut.GetComponent<MeshFilter>();
+		Mesh mesh = meshFilter.sharedMesh;
+
+		/*foreach(Vector3 vec in mesh.vertices)
+		{
+			Debug.Log(vec);
+		}*/
+		//Debug.Log("vertices: "+mesh.vertices.Length+", triangles: "+mesh.triangles.Length);
+
+		GameObject firstPiece = new GameObject(gameObjectToCut.name+"-FirstPiece");
+		MeshFilter firstPieceMeshFilter = firstPiece.AddComponent<MeshFilter>();
+		firstPiece.AddComponent<MeshRenderer>();
+
+		firstPieceMeshFilter.sharedMesh = new Mesh();
+		firstPieceMeshFilter.sharedMesh.vertices = new Vector3[mesh.vertices.Length];
+
+		int i = 0;
+		foreach(Vector3 vec in mesh.vertices)
+		{
+			if(mesh.vertices[i].x < 0)
+			{
+				firstPieceMeshFilter.sharedMesh.vertices[i] = mesh.vertices[i];
+			}
+			i++;
+		}
+	}
+}
---
+++ Assets/Scripts/MeshCutter/readme.txt
@@ -0,0 +1,5 @@
+Mesh Cutter was created by github.com/hugoscurti
+
+&
+
+Modified for the "Save the Rabbits" game by github.com/dawid-t
