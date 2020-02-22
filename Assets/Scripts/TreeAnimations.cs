27b89bfb8d80d3e30c60b1d67e57d5a901fda06c
dawid-t <dawiddt@wp.pl>
2020-02-22 02:44:20 +0100

    Add tree animations

---
+++ Assets/Scripts/TreeAnimations.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class TreeAnimations : MonoBehaviour
+{
+	[SerializeField]
+	private int treeNumber;
+
+
+	private void Awake()
+	{
+		GetComponent<Animator>().Play("Tree-"+treeNumber, 0, 0);
+	}
+}
